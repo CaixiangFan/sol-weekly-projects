bdb8851dd210ad36a331e281a1c0c9eab3e03c34
caixiangfan <caixiang@ualberta.ca>
2022-06-02 18:21:48 +0000

    updated API title

--- week3/Project/src/main.ts
+++ week3/Project/src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const swaggerOptions = new DocumentBuilder()
-    .setTitle('Lesson 16 Project')
+    .setTitle('Week3 Project')
     .setVersion('1.0.0')
-    .setDescription('Encode Club Bootcamp May Project for Lesson 16')
+    .setDescription('Encode Club Bootcamp May Project of Team F')
     .build();
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('docs', app, document);
